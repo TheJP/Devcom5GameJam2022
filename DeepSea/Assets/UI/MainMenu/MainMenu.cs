5843c2474aa522ccad89dcb70c102d03df9cc773
TheJP <jp_55555@hotmail.com>
2025-05-24 09:03:03 +0200

    Added anti burn-in flicker
    Fills the full screen with a random colour every 10 minutes.

--- DeepSea/Assets/UI/MainMenu/MainMenu.cs
+++ DeepSea/Assets/UI/MainMenu/MainMenu.cs
@@ -1,15 +1,41 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
+[RequireComponent(typeof(UIDocument))]
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private float antiBurnInInterval = 10.0f;
+
+    private UIDocument uiDocument;
+    private VisualElement antiBurnIn;
+
+    private void Start()
+    {
+        uiDocument = GetComponent<UIDocument>();
+        antiBurnIn = uiDocument.rootVisualElement.Q("anti-burn-in");
+        StartCoroutine(AntiBurnInFlicker());
+    }
+
     private void Update()
     {
         if (Input.anyKey)
         {
             SceneManager.LoadScene(1);
+        }
+    }
+
+    private IEnumerator AntiBurnInFlicker()
+    {
+        while (true)
+        {
+            antiBurnIn.style.backgroundColor = Random.ColorHSV();
+            antiBurnIn.style.display = new StyleEnum<DisplayStyle>(DisplayStyle.Flex);
+            yield return new WaitForSeconds(0.5f);
+            antiBurnIn.style.display = new StyleEnum<DisplayStyle>(DisplayStyle.None);
+            yield return new WaitForSeconds(antiBurnInInterval);
         }
     }
 }
